b4875e65d5f31ba101bd70a7601c3e4a3922ba1a
Maya Ausset <mayaausset@gmail.com>
2021-03-25 11:33:32 +0100

    main styling and layout

--- client/src/components/MovieImage.js
+++ client/src/components/MovieImage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import NoPicture from "../assets/no-picture.png";
 
 const MovieImage = ({poster}) => {
-  return <div><img src={`https://image.tmdb.org/t/p/original/${poster}`} alt="" width="200px"/></div>
+  return (
+    <div>
+    { poster != null ? (
+      <img src={`https://image.tmdb.org/t/p/original/${poster}`} alt="poster"/>
+    ) : (
+      <img src={NoPicture} alt="Icon made by Freepik from www.flaticon.com" width="100px"/>
+    ) }
+    </div>
+  ) 
 };
 
 export default MovieImage;
