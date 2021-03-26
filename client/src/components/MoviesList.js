bb40c939e0c78dd1b9d6a45ce6ffe31361305740
Maya Ausset <mayaausset@gmail.com>
2021-03-26 12:11:02 +0100

    refactoring, code cleaning

--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -1,49 +1,30 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { motion } from "framer-motion";
+import NoMoviesFound from "./NoMoviesFound";
+import SelectableMovieTitle from "./SelectableMovieTitle";
 
 const MoviesList = ({ movies, handleSelectionClick }) => {
   return (
     <div className="moviesList">
-    <Container fluid className="scrollable-content">
-      {movies.length !== 0 ? (
-        <Row>
-          {movies.map((movie) => {
-            return (
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                key={movie.id}
-                className="movie-item"
-              >
-                <div key={movie.id}>
-                  <button
-                    className="movie-link"
-                    onClick={() => {
-                      handleSelectionClick(movie);
-                    }}
-                  >
-                    {movie.title}
-                  </button>
-                </div>
-              </motion.div>
-            );
-          })}
-        </Row>
-      ) : (
-        <Row>
-          <div className="movie-item">
-            <div className="movie-item">
-              <button className="movie-link">
-                No movies found with that title...
-              </button>
-            </div>
-          </div>
-        </Row>
-      )}
-    </Container>
+      <Container fluid className="scrollable-content">
+        {movies.length !== 0 ? (
+          <Row>
+            {movies.map((movie) => {
+              return (
+                <SelectableMovieTitle
+                  movie={movie}
+                  handleSelectionClick={handleSelectionClick}
+                />
+              );
+            })}
+          </Row>
+        ) : (
+          <Row>
+            <NoMoviesFound />
+          </Row>
+        )}
+      </Container>
     </div>
   );
 };
